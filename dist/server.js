a3da09c137c8a2b67eb42b51b7e24755db5ab906
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2025-03-25 23:15:15 +0200

    change dist

--- dist/server.js
+++ dist/server.js
@@ -3,12 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const data_source_1 = require("../src/config/data-source");
+require("reflect-metadata");
+const data_source_1 = require("./config/data-source");
 const supabase_config_1 = require("./config/supabase.config");
-const express_1 = __importDefault(require("express"));
+const app_1 = __importDefault(require("./app"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const app = (0, express_1.default)();
 const PORT = process.env.SUPABASE_PORT || 5000;
 data_source_1.AppDataSource.initialize()
     .then(async () => {
@@ -17,7 +17,7 @@ data_source_1.AppDataSource.initialize()
         console.error("Failed to connect to Supabase");
         process.exit(1);
     }
-    app.listen(PORT, () => {
+    app_1.default.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 })
